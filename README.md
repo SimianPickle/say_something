7184060970fed27128d3d88a689eb5a5eb35acfb
Pickle <pickle@DESKTOP-LR6UVUQ.localdomain>
2022-09-01 23:35:12 -0400

    first commit readme and requirements text

---
+++ README.md
@@ -0,0 +1,3 @@
+## Python Program 
+
+This is a Python program.
---
+++ main.py
@@ -0,0 +1,11 @@
+def say_something(word):
+    """
+    Prints to console the word variable
+    """
+    print(word)
+
+
+if __name__ == "__main__": 
+    something = input("Enter word here: ")
+
+    say_something(something)
